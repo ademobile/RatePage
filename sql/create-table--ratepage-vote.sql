f9fd9ff254c90e60d2391350041928bbff65676e
Ostrzyciel <magwac69@gmail.com>
2019-04-19 11:18:18 +0200

    support for SQLite

--- sql/create-table--ratepage-vote.sql
+++ sql/create-table--ratepage-vote.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS ratepage_vote (
-  `rv_page_id` int(10) unsigned NOT NULL,
+CREATE TABLE IF NOT EXISTS /*_*/ratepage_vote (
+  `rv_page_id` int(10) unsigned NOT NULL default NULL,
   `rv_user` varchar(255) NOT NULL default '',
   `rv_ip` varchar(255) default NULL,
-  `rv_answer` int(3) NOT NULL,
-  `rv_date` datetime NOT NULL,
+  `rv_answer` int(3) NOT NULL default NULL,
+  `rv_date` datetime NOT NULL default NULL,
   PRIMARY KEY  (`rv_page_id`,`rv_user`)
-);
+) /*$wgDBTableOptions*/;
