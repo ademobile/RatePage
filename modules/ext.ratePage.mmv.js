9127408a0dd058f942b2cd01be08bd49049c1331
Ostrzyciel nożyczek <ostrzycielnozyczek@gmail.com>
2020-04-12 16:51:00 +0200

    WIP MMV widget

--- modules/ext.ratePage.mmv.js
+++ modules/ext.ratePage.mmv.js
@@ -1,9 +1,18 @@
 $(document).ready( function () {
+	function addRpWidget() {
+		
+	}
+
 	function mutationCallback(mutationList, observer) {
 		mutationList.forEach((mutation) => {
-			// mutation.addedNodes
+			mutation.addedNodes.forEach( function (node) {
+				if ( node.nodeType === Node.ELEMENT_NODE && node.classList.contains('mw-mmv-wrapper') ) {
+					setTimeout(addRpWidget, 500);   // give mmv time to set up or something
+				}
+			} )
 		});
 	}
 
-	// do stuff
+	var observer = new MutationObserver(mutationCallback);
+	observer.observe(document.body, {childList: true});
 } );
